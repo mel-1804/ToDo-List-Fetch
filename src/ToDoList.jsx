c4cfce0f744fb3ad1ed94102ce1c547551df56dd
Melissa Ortiz <melissa.ortiz.m88@gmail.com>
2024-08-31 09:52:38 -0400

    proyecto terminado

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,31 +1,104 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 export function ToDoList() {
 
-    const [valorActividad, setValorActividad] = useState("");
+    const [valorActividad, setValorActividad] = useState({ label: "", is_done: false });
     const [arregloTareas, setArregloTareas] = useState([]);
-    
+    const [data, setData] = useState({ name: "", todos: [] })
+    const [button, setButton] = useState(false)
+
     const handlerEnter = (e) => {
+        console.log(data.name)
         if (e.keyCode === 13) {
-            if(!arregloTareas.find((tarea)=>tarea==valorActividad)){
-                setArregloTareas([...arregloTareas, valorActividad])
-                setValorActividad("")
-            }
+
+            let url = `https://playground.4geeks.com/todo/todos/${data.name}`;
+            let options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(valorActividad)
+            };
+
+            fetch(url, options)
+                .then((respuesta) => {
+                    if (respuesta.ok) {
+                        return respuesta.json();
+                    } else {
+                        throw new Error("Hubo un error");
+                    }
+                })
+                .then((result) => {
+                    getUserTodos()
+                })
+                .catch((error) => console.log(error));
+
+            setValorActividad({ label: "", is_done: false })
         }
     }
 
+
     const handlerClick = (tarea) => {
-        const nuevoArregloTareas = arregloTareas.filter((item)=>item!=tarea)
-        setArregloTareas(nuevoArregloTareas)
-        console.log("ejecutando")
-        }
-   
-  
+        let url = `https://playground.4geeks.com/todo/todos/${tarea.id}`;
+        let options = {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        };
+
+        fetch(url, options)
+            .then((respuesta) => {
+            // console.log(respuesta)
+            getUserTodos()
+            })
+            .catch((error) => console.log(error));
+
+        setValorActividad({ label: "", is_done: false })
+    }
+
+
+    const getUserTodos = () => {
+        fetch(`https://playground.4geeks.com/todo/users/${data.name}`)
+            .then((respuesta) => {
+                // console.log(respuesta);
+                if (respuesta.ok) {
+                    return respuesta.json();
+                } else {
+                    throw new Error("Hubo un error");
+                }
+            })
+            .then((result) => {
+                // console.log(result)
+                setData(result);
+                setArregloTareas(result.todos)
+            })
+            .catch((error) => console.log(error));
+    }
+
+
+
 
 
     return (
         <div className="fondo d-flex justify-content-center">
             <div className="papel rounded-4 border border-4 p-4">
                 <p className="titulo">ToDo<i className="mancuerna fa-solid fa-dumbbell"></i>List</p>
+                <div className="usuario">User:
+                    <input type="text"
+                        className="valorUsuario"
+                        placeholder="Enter your user"
+                        aria-label="entrada"
+                        aria-describedby="basic-addon1"
+                        value={data.name}
+                        onChange={(e) => setData({
+                            ...data,
+                            name: e.target.value
+                        })}>
+                    </input>
+                    <button className="botonAcceso"
+                        onClick={() => getUserTodos()}
+                    >Login</button>
+                </div>
                 <div className="input-group mb-3">
                     <input type="text"
                         className="entrada"
@@ -33,16 +106,16 @@ export function ToDoList() {
                         placeholder="Enter your work out"
                         aria-label="entrada"
                         aria-describedby="basic-addon1"
-                        value={valorActividad}
-                        onChange={(e) => setValorActividad(e.target.value)}
+                        value={valorActividad.label}
+                        onChange={(e) => setValorActividad({ label: e.target.value, is_done: false })}
                         onKeyDown={(e) => handlerEnter(e)} />
                 </div>
                 <div>
                     <ul className="listaActividades">
-                        {arregloTareas.map((tarea, i) => {
+                        {arregloTareas?.map((tarea, i) => {
                             return (
                                 <div key={i} className="listaActividades d-flex justify-content-between">
-                                    <li className="actividad">{tarea}</li>
+                                    <li className="actividad">{tarea.label}</li>
                                     <button className="eliminar"
                                         onClick={() => handlerClick(tarea)}>
                                         x
@@ -52,7 +125,9 @@ export function ToDoList() {
                         })}
                     </ul>
                 </div>
-                <div className="contador">{arregloTareas.length} PENDING ACTIVITIES</div>
+                <div className="contador">
+                    {arregloTareas?.length} PENDING ACTIVITIES
+                </div>
             </div>
         </div>
     )
